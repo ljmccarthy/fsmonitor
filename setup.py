2e96ea913df2b56313849cbc1d2ed1bd963f5ccc
Luke McCarthy <luke@iogopro.co.uk>
2010-03-20 13:11:04 +0000

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "fsmonitor",
+    version = "0.1",
+    description = "Filesystem monitoring",
+    author = "Luke McCarthy",
+    author_email = "luke@iogopro.co.uk",
+    url = "http://",
+    packages = ["fsmonitor"],
+)
