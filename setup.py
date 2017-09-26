75186b301aa76556b89e34c389eb9ae331c7dd63
asettouf <adonis.settouf@gmail.com>
2017-09-26 22:27:30 +0200

    Add long description in setup.py for push to Pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
+from os.path import exists
 
 setup(
     name = "fsmonitor",
     version = "0.1",
     description = "Filesystem monitoring",
+    long_description=(open('README.rst').read() if exists('README.rst')
+                      else ''),
     author = "Luke McCarthy",
     author_email = "luke@iogopro.co.uk",
+    licence="MIT",
     url = "http://github.com/shaurz/fsmonitor",
     install_requires=[
         'pypiwin32',
