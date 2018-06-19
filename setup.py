756185951fd88fbffae974f1e5f8ab71806ab61a
Oleg Broytman <phd@phdru.name>
2018-06-20 01:17:56 +0300

    Feat(setup.py): Add more PyPI params: URLs, keywords, platforms

--- setup.py
+++ setup.py
@@ -7,16 +7,26 @@ requires = []
 if system == "Windows":
     requires = ["pypiwin32"]
 
+version = "0.1"
+
 setup(
     name="fsmonitor",
-    version="0.1",
+    version=version,
     description="Filesystem monitoring",
     long_description=open('README.rst').read(),
     long_description_content_type="text/x-rst",
     author="Luke McCarthy",
     author_email="luke@iogopro.co.uk",
     licence="MIT",
-    url="http://github.com/shaurz/fsmonitor",
+    url="http://github.com/ljmccarthy/fsmonitor",
+    download_url="https://pypi.org/project/fsmonitor/%s/" % version,
+    project_urls={
+        'Homepage': 'http://github.com/ljmccarthy/fsmonitor',
+        'Download': 'https://pypi.org/project/fsmonitor/%s/' % version,
+        'Issue tracker': 'http://github.com/ljmccarthy/fsmonitor/issues',
+    },
+    keywords=["filesystem", "monitor"],
+    platforms="Any",
     install_requires=requires,
     packages=["fsmonitor"],
 )
