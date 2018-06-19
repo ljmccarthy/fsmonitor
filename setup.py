a6f09c6968d78fc6802244d09161d4e91825d8e4
Oleg Broytman <phd@phdru.name>
2018-06-20 01:02:27 +0300

    Style(setup.py): Remove spaces around `=`

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ if system == "Windows":
     requires = ["pypiwin32"]
 
 setup(
-    name = "fsmonitor",
-    version = "0.1",
-    description = "Filesystem monitoring",
+    name="fsmonitor",
+    version="0.1",
+    description="Filesystem monitoring",
     long_description=(open('README.rst').read() if exists('README.rst')
                       else ''),
-    author = "Luke McCarthy",
-    author_email = "luke@iogopro.co.uk",
+    author="Luke McCarthy",
+    author_email="luke@iogopro.co.uk",
     licence="MIT",
-    url = "http://github.com/shaurz/fsmonitor",
+    url="http://github.com/shaurz/fsmonitor",
     install_requires=requires,
-    packages = ["fsmonitor"],
+    packages=["fsmonitor"],
 )
