651d64bcb1191da544fe8155ecf28608a121999d
Oleg Broytman <phd@phdru.name>
2018-06-26 23:24:01 +0300

    Test(test3): On w32 PY2 and PY3 have different error codes

--- tests/test_3_errorno_enoent.py
+++ tests/test_3_errorno_enoent.py
@@ -1,12 +1,16 @@
-import os, time, errno
+import sys, os, time, errno
 from utils import *
 from fsmonitor import *
+from fsmonitor.compat import PY3
 
 def test_3_errorno_enoent():
     fsm_test = FSMonitorTest()
+    err_code = errno.ENOENT
+    if PY3 and sys.platform == "win32":
+        err_code = errno.ESRCH
     try:
         fsm_test.add_dir_watch("/this/path/does/not/exist")
     except FSMonitorError as e:
-        assert e.errno == errno.ENOENT
+        assert e.errno == err_code
     else:
         assert False, "Expected exception"
